8900a1562a220f9c2853356608c47618650e7c04
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-10-13 18:33:08 +0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+//Header file
 
 using namespace std;
+// Allows the program use inbuilt variables and objects 
 
 int main() {
-
+//Output to the screen
   cout << "Hello World!" << endl;
 
   cout << "I am learning C++";
 
   return 0;
-
+  //Terminate the program
 }
 
